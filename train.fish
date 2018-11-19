e1c6c70a6728edabdada3a16cb1c6f49d722fb7b
feilong <feilongphone@gmail.com>
2018-11-19 13:28:30 +0800

    2018年 11月 19日 星期一 13:28:30 CST

--- train.fish
+++ train.fish
@@ -1,18 +1,21 @@
 #!/usr/bin/env fish
 
-set lineCount (wc -l ./data/news.des.label.save)
-set traindatanum (expr 0.9 * $lineCount)
-set testDataNum (expr $lineCount - $traindatanum)
-
 set oridata ./data/news.title.label.save
+set tempdata /tmp/news.title.label.save
 set traindata /tmp/train.data
 set testdata /tmp/test.data
 set modeldir ./model/news.model
 
+set lineCount (wc -l $oridata | cut -d ' ' -f 1)
+set traindatanum (expr 95 \* $lineCount / 100)
+set testDataNum (expr $lineCount - $traindatanum)
+
 echo get $lineCount data, $traindatanum for train, $testDataNum for test.
 
-head -n $traindatanum $oridata > $traindata 
-tail -n $testDataNum $oridata > $testdata 
+cat $oridata > $tempdata 
+
+head -n $traindatanum $tempdata > $traindata 
+tail -n $testDataNum $tempdata > $testdata 
 
 fasttext supervised -input $traindata -output $modeldir -epoch 50 -lr 1.0 -wordNgrams 2
 fasttext test $modeldir.bin $testdata
