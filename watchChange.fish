43fef9e3128d08281185f03200f64fc3693fd6ca
feilong <feilongphone@gmail.com>
2018-11-19 11:56:29 +0800

    2018年 11月 19日 星期一 11:56:29 CST

--- watchChange.fish
+++ watchChange.fish
@@ -19,10 +19,15 @@ echo label ...
 #
 #
 echo dedu ...
-cat ./data/news.title.label ./data/news.title.label.save | sort -n | uniq | tee ./data/news.title.label.save >/dev/null
-cat ./data/news.des.label ./data/news.des.label.save | sort -n | uniq | tee ./data/news.des.label.save >/dev/null
-cat ./data/news.title.ori ./data/news.title.origin.save | sort -n | uniq | tee ./data/news.title.origin.save >/dev/null
-cat ./data/news.url ./data/news.url.save | sort -n | uniq | tee ./data/new.url.save >/dev/null
+cat ./data/news.title.label ./data/news.title.label.save | sort -n | uniq | tee ./data/news.title.label.save.tmp >/dev/null
+cat ./data/news.des.label ./data/news.des.label.save | sort -n | uniq | tee ./data/news.des.label.save.tmp >/dev/null
+cat ./data/news.title.ori ./data/news.title.origin.save | sort -n | uniq | tee ./data/news.title.origin.save.tmp >/dev/null
+cat ./data/news.url ./data/news.url.save | sort -n | uniq | tee ./data/new.url.save.tmp >/dev/null
+
+cp ./data/news.title.label.save.tmp ./data/news.title.label.save
+cp ./data/news.des.label.save.tmp ./data/news.des.label.save
+cp ./data/news.title.origin.save.tmp ./data/news.title.origin.save
+cp ./data/news.url.save.tmp ./data/news.url.save
 
 git add ./data/*.save
 git commit -m (date)
